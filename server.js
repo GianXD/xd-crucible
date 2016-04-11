7176cd74b6c83be02735ec5257311053a8fe3d68
Gian Mancuso <Giancarlo.Mancuso@d2l.com>
2016-04-11 09:17:27 -0400

    Testing out stormpath

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var mongoose    = require('mongoose');
 var stormpath 	= require('express-stormpath');
 
 var jwt    		= require('jsonwebtoken'); // used to create, sign, and verify tokens
-var config 		= require('./config'); // get our config file
+//var config 		= require('./config'); // get our config file
 //var User   		= require('./app/models/user'); // get our mongoose model
     
 // =======================
@@ -17,8 +17,8 @@ var config 		= require('./config'); // get our config file
 // =======================
 
 var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
-mongoose.connect(config.database); // connect to database
-app.set('superSecret', config.secret); // secret variable
+//mongoose.connect(config.database); // connect to database
+//app.set('superSecret', config.secret); // secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,70 +31,21 @@ app.use(morgan('dev'));
 // routes ================
 // =======================
 // basic route
+
 app.get('/', function(req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
 // API ROUTES -------------------
-
-// get an instance of the router for api routes
-var apiRoutes = express.Router(); 
-
-// STORMPATH Middlewear must be the last initialized middlewear
 app.use(stormpath.init(app, {
   website: true
 }));
 
-// route to authenticate a user (POST http://localhost:8080/api/authenticate)
-apiRoutes.post('/authenticate', function(req, res) {
+var apiRoutes = express.Router(); 
 
-  // find the user
-  User.findOne({
-    name: req.body.name
-  }, function(err, user) {
-
-    if (err) throw err;
-
-    if (!user) {
-      res.json({ success: false, message: 'Authentication failed. User not found.' });
-    } else if (user) {
-
-      // check if password matches
-      if (user.password != req.body.password) {
-        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-      } else {
-
-        // if user is found and password is right
-        // create a token
-        var token = jwt.sign(user, app.get('superSecret'), {
-          expiresInMinutes: 1440 // expires in 24 hours
-        });
-
-        // return the information including token as JSON
-        res.json({
-          success: true,
-          message: 'Enjoy your token!',
-          token: token
-        });
-      }   
-
-    }
-
-  });
+apiRoutes.get('/yup', function(req,res) {
+	res.send('Yup, API');
 });
-
-// route to show a random message (GET http://localhost:8080/api/)
-apiRoutes.get('/', function(req, res) {
-  res.json({ message: 'Welcome to the coolest API on earth!' });
-});
-
-// route to return all users (GET http://localhost:8080/api/users)
-apiRoutes.get('/users', function(req, res) {
-  User.find({}, function(err, users) {
-    res.json(users);
-  });
-});
-
 
 // apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
@@ -102,5 +53,7 @@ app.use('/api', apiRoutes);
 // =======================
 // start the server ======
 // =======================
-app.listen(port);
-console.log('Crucible happening at http://localhost:' + port);
+app.on('stormpath.ready', function() {
+  app.listen(port);
+  console.log('Crucible happening at http://localhost:' + port);
+});
